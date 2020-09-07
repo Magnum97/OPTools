32e53150f5d10472ed6dbf099b5e829002198022
Richard Simpson <39318657+Magnum97@users.noreply.github.com>
2020-09-07 12:54:44 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Foundation Starter
+
+This is just a framework to simplify future [Minecraft](http://www.minecraft.net) plugin creation using the [Foundation](http://www.github.com/Magnum97/Foundation) framework.
---
+++ src/main/java/me/magnum/mc/FoundationStarter.java
@@ -0,0 +1,11 @@
+package me.magnum.mc;
+
+import org.mineacademy.fo.plugin.SimplePlugin;
+
+public class FoundationStarter extends SimplePlugin {
+
+	@Override
+	protected void onPluginStart () {
+		// Plugin startup
+	}
+}
